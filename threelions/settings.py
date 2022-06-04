d6846a5c496cec1667324366feb262d946a9176a
ConRdav <connor@redwoodglade.com>
2022-06-04 11:21:39 +0000

    Add Comment model and migrate models to database

--- threelions/settings.py
+++ threelions/settings.py
@@ -134,7 +134,7 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
