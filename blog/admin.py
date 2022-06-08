b297679a22c07b268dc5b3a8baec5811843ecf5e
ConRdav <connor@redwoodglade.com>
2022-06-08 20:45:36 +0000

    Migrated files after Heroku database update

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,7 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status','created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
     list_filter = ('status', 'created_on')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
