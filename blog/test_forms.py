ec9206955c7cd2e10cb715bce8c4eab33553dc41
ConRdav <connor@redwoodglade.com>
2022-06-09 16:55:43 +0000

    Add TestBlogForm class to tests_forms.py

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .forms import CommentForm
+from .forms import CommentForm, BlogForm
 
 
 class TestCommentForm(TestCase):
@@ -8,10 +8,31 @@ class TestCommentForm(TestCase):
         form = CommentForm(({'body': ''}))
         self.assertFalse(form.is_valid())
         self.assertIn('body', form.errors.keys())
-        self.assertEqual(form.errors['body'][0], 'This field is required')
+        self.assertEqual(form.errors['body'][0], 'This field is required.')
 
     def test_fields_are_explicit_in_form_metaclass(self):
         form = CommentForm()
         self.assertEqual(
             form.Meta.fields, ('body',)
         )
+
+
+class TestBlogForm(TestCase):
+    def test_post_title_is_required(self):
+        form = BlogForm(({'title': ''}))
+        self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors.keys())
+        self.assertEqual(form.errors['title'][0], 'This field is required.')
+    
+    def test_post_content_is_required(self):
+        form = BlogForm(({'content': ''}))
+        self.assertFalse(form.is_valid())
+        self.assertIn('content', form.errors.keys())
+        self.assertEqual(form.errors['content'][0], 'This field is required.')
+    
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = BlogForm()
+        self.assertEqual(
+            form.Meta.fields, ('title', 'content', 'excerpt')
+        )
+    
--- threelions/settings.py
+++ threelions/settings.py
@@ -98,14 +98,18 @@ WSGI_APPLICATION = 'threelions.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
-DATABASES = {
+#testing database
+if 'TESTING' in os.environ:
+    print('Using sqlite3 db')
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
