75f36abf3b216e4f6c45894ccfb364933928d0d4
ConRdav <connor@redwoodglade.com>
2022-06-09 13:39:52 +0000

    Add delete post url path to urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('create_post', views.CreatePost.as_view(), name='create_post'),
     path('user_posts', views.user_posts, name='user_posts'),
     path('edit/<post_id>', views.edit_post, name='edit_post'),
+    path('delete/<post_id>', views.delete_post, name='delete_post'),
 ]
