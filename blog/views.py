862e2d164a9eea604189c683217cc55228042f53
ConRdav <connor@redwoodglade.com>
2022-06-09 11:50:47 +0000

    Update views.py PostList class to use apporved instead of status

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ from .forms import CommentForm, BlogForm
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('created_on')
+    queryset = Post.objects.filter(approved=True).order_by('created_on')
     template_name = 'index.html'
     paginate_by = 6
 
