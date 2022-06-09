ad3d577620dc82712f3bd9c15160dc60fca8cae2
ConRdav <connor@redwoodglade.com>
2022-06-09 09:04:54 +0000

    update urls.py to include CreatePost class

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('create_post', views.createPost, name='create_post'),
-    path('team', views.team, name='team'),
+    path('create_post', views.CreatePost, name='create_post'),
 ]
