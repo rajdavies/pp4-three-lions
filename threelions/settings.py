a9342d60435642f254b1c32807f817a44f2621e4
ConRdav <connor@redwoodglade.com>
2022-06-10 05:23:33 +0000

    Update base.html styling

--- threelions/settings.py
+++ threelions/settings.py
@@ -163,8 +163,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_DIRS = [os.path.join(BASE_DIR, 'static/')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
